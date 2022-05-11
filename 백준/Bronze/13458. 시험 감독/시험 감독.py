ec1485cdf81a58b97e95f6192652a76009831466
Hyunsu Shin <58941022+scato3@users.noreply.github.com>
2022-05-11 13:24:22 +0900

    [Bronze II] Title: 시험 감독, Time: 404 ms, Memory: 227880 KB -BaekjoonHub

--- 백준/Bronze/13458. 시험 감독/시험 감독.py
+++ 백준/Bronze/13458. 시험 감독/시험 감독.py
@@ -1,20 +1,17 @@
 n = int(input())
-
-arr = list(map(int, input().split()))
-
+a = list(map(int, input().split()))
 b, c = map(int, input().split())
 
 sum = 0
-
 for i in range(n):
-    if arr[i] > 0:
-        arr[i] -= b
+    if a[i] > 0:
+        a[i] -= b
         sum += 1
-    
-    if arr[i] > 0:
-        sum += (arr[i] // c)
         
-        if arr[i] % c != 0:
+    if a[i] > 0:
+        sum += a[i] // c
+        
+        if a[i] % c != 0:
             sum += 1
 
 print(sum)
