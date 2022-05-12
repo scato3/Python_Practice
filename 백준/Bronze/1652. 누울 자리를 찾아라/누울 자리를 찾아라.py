84ebea0847d885610018ba7dea36e0ccde3ebad1
Hyunsu Shin <58941022+scato3@users.noreply.github.com>
2022-05-12 11:26:08 +0900

    [Bronze I] Title: 누울 자리를 찾아라, Time: 124 ms, Memory: 114488 KB -BaekjoonHub

--- 백준/Bronze/1652. 누울 자리를 찾아라/누울 자리를 찾아라.py
+++ 백준/Bronze/1652. 누울 자리를 찾아라/누울 자리를 찾아라.py
@@ -1,26 +1,33 @@
 n = int(input())
 graph = []
-
 for _ in range(n):
-    graph.append(input() + 'X')
-graph.append('X' * (n+1))
+    graph.append(list(input()))
+    
+w_data = 0
+h_data = 0
 
-w, h = 0, 0
-for i in range(n+1):
-    w_data, h_data = 0, 0
-    for j in range(n+1):
-        if graph[i][j] == '.':
+for i in range(n):
+    cnt = 0
+    for j in range(n):
+        if graph[i][j] == '.' :
+            cnt += 1
+        
+        if graph[i][j] == 'X':
+            cnt = 0
+        
+        if cnt == 2:
             w_data += 1
-        elif graph[i][j] == 'X':
-            if w_data >= 2:
-                w += 1
-            w_data = 0
-        if graph[j][i] == '.':
+            
+for i in range(n):
+    cnt = 0
+    for j in range(n):
+        if graph[j][i] == '.' :
+            cnt += 1
+        
+        if graph[j][i] == 'X':
+            cnt = 0
+        
+        if cnt == 2:
             h_data += 1
-        elif graph[j][i] == 'X':
-            if h_data >= 2:
-                h += 1
-            h_data = 0
 
-print(w)
-print(h)
+print(w_data, h_data)
