b3f24ce98b14eb0fae03b80281d42084f241a415
Hyunsu Shin <58941022+scato3@users.noreply.github.com>
2022-05-14 14:11:29 +0900

    [Silver V] Title: 체스판 다시 칠하기, Time: 144 ms, Memory: 115408 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
@@ -1,11 +1,12 @@
 n, m = map(int, input().split())
 graph = []
+
 for _ in range(n):
     graph.append(input())
-    
-ans = []
-for i in range(n-7):
-    for j in range(m-7):
+   
+arr = []
+for i in range(n - 7):
+    for j in range(m - 7):
         w_data = 0
         b_data = 0
         for k in range(i, i+8):
@@ -16,10 +17,10 @@ for i in range(n-7):
                     if graph[k][l] == 'B':
                         b_data += 1
                 else:
-                    if graph[k][l] =='B':
+                    if graph[k][l] == 'B':
                         w_data += 1
-                    if graph[k][l] =='W':
+                    if graph[k][l] == 'W':
                         b_data += 1
-        ans.append(min(w_data, b_data))
-        
-print(min(ans))
+        arr.append(min(w_data, b_data))
+
+print(min(arr))
