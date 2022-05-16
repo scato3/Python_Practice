efa32aa93397ecdce6a5ec2386df1ff6bfff8d76
Hyunsu Shin <58941022+scato3@users.noreply.github.com>
2022-05-16 12:08:09 +0900

    [Bronze I] Title: 임시 반장 정하기, Time: 400 ms, Memory: 122912 KB -BaekjoonHub

---
+++ 백준/Bronze/1268. 임시 반장 정하기/임시 반장 정하기.py
@@ -0,0 +1,19 @@
+n = int(input())
+graph = []
+visited = [0] * n
+for i in range(n):
+    graph.append(list(map(int, input().split())))
+    visited[i] = [0] * n
+
+for i in range(5): # 반
+    for j in range(n): # 학생
+        for k in range(j+1, n):
+            if graph[j][i] == graph[k][i]:
+                visited[j][k] = 1
+                visited[k][j] = 1
+cnt = []
+
+for i in visited:
+    cnt.append(i.count(1))
+
+print(cnt.index(max(cnt)) + 1)
