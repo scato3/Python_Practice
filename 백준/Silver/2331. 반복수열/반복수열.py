2ecb43cdc1ae781a26a387ca21514f104c0b019d
Hyunsu Shin <58941022+scato3@users.noreply.github.com>
2022-05-20 14:48:38 +0900

    [Silver IV] Title: 반복수열, Time: 108 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Silver/2331. 반복수열/반복수열.py
@@ -0,0 +1,14 @@
+a, b = map(int, input().split())
+nums = [a]
+
+while True:
+    num = 0
+    for i in str(nums[-1]):
+        num += int(i) ** b
+    
+    if num in nums:
+        break
+    else:
+        nums.append(num)
+
+print(nums.index(num))
