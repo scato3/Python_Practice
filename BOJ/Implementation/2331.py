6d31d7b163e0e9c081825ab393b9fa6ddcc52af9
Hyun Su <tlsgustn93@naver.com>
2022-05-20 15:41:52 +0900

    BOJ

--- BOJ/Implementation/2331.py
+++ BOJ/Implementation/2331.py
@@ -1,14 +1,14 @@
 a, b = map(int, input().split())
-nums = [a] # 최초의 값 넣어준다.
+nums = [a]
 
 while True:
-    num = 0 # 자릿수의 b 제곱 값을 더할 변수를 지정
-    for i in str(nums[-1]): # 마지막에 들어온 값을 처리하기 위해 str 처리
+    num = 0
+    for i in str(nums[-1]):
         num += int(i) ** b
-    
+
     if num in nums:
         break
     else:
-        nums.append(num) # 반복되지 않는다면 nums에 append 해준다.
+        nums.append(num)
 
 print(nums.index(num))
--- BOJ/Implementation/2477.py
+++ BOJ/Implementation/2477.py
@@ -1,21 +1,21 @@
-melon = int(input())
+n = int(input())
+board = [list(map(int, input().split())) for _ in range(6)]
+w = 0
+w_idx = 0
+h = 0
+h_idx = 0
 
-w, w_idx = 0, 0
-h, h_idx = 0, 0
-
-arr = [list(map(int, input().split())) for _ in range(6)]
-    
-for i in range(len(arr)):
-    if arr[i][0] == 1 or arr[i][0] == 2:
-        if arr[i][1] > w :
-            w = arr[i][1]
+for i in range(6):
+    if board[i][0] == 1 or board[i][0] == 2:
+        if w < board[i][1]:
+            w = board[i][1]
             w_idx = i
-    elif arr[i][0] == 3 or arr[i][0] == 4:
-        if arr[i][1] > h :
-            h = arr[i][1]
+    if board[i][0] == 3 or board[i][0] == 4:
+        if h < board[i][1]:
+            h = board[i][1]
             h_idx = i
 
-subW = abs(arr[(w_idx - 1) % 6][1] - arr[(w_idx + 1) % 6][1])
-subH = abs(arr[(h_idx - 1) % 6][1] - arr[(h_idx + 1) % 6][1])
+W = abs(board[(w_idx - 1) % 6][1] - board[(w_idx + 1) % 6][1])
+H = abs(board[(h_idx - 1) % 6][1] - board[(h_idx + 1) % 6][1])
 
-print((w * h - subW * subH) * melon)
+print(((w * h) - (W * H)) *n)
