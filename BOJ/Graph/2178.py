62d978bd83f49d1b427ef26c622ed7a82f631173
Hyun Su <tlsgustn93@naver.com>
2022-05-21 16:08:11 +0900

    BOJ

--- BOJ/Graph/2178.py
+++ BOJ/Graph/2178.py
@@ -1,26 +1,31 @@
+from collections import deque
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
 n, m = map(int, input().split())
-board = []
+graph = []
 for _ in range(n):
-    board.append(list(map(int, input()))) # 미로 만들기
-    
-visited = [[0] * m for _ in range(n)] # 방문했는지 확인
-dx = [1, 0, -1, 0]
-dy = [0, 1, 0, -1]
+    graph.append(list(map(int, input())))
 
-queue = [(0, 0)]
-visited[0][0] = 1
+visited = [[0] * m for _ in range(n)]
 
-while queue:
-    x, y = queue.pop(0)
-    
-    if x == n - 1 and y == m -1:
-        print(visited[x][y])
-        break
-    for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
-        if 0 <= nx < n and 0 <= ny < m:
-            if visited[nx][ny] == 0 and board[nx][ny] == 1:
-                visited[nx][ny] = visited[x][y] + 1
-                queue.append((nx, ny))
+def bfs():
+    q = deque()
+    q.append((0, 0))
+    visited[0][0] = 1
 
+    while q:
+        x, y = q.popleft()
+
+        if x == n - 1 and y == m - 1:
+            return visited[x][y]
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if 0 <= nx < n and 0 <= ny < m:
+                if visited[nx][ny] == 0 and graph[nx][ny] == 1:
+                    visited[nx][ny] = visited[x][y] + 1
+                    q.append((nx, ny))
+
+print(bfs())
--- BOJ/Graph/2636.py
+++ BOJ/Graph/2636.py
@@ -1,49 +1,44 @@
 from collections import deque
-
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
-
 n, m = map(int, input().split())
 graph = []
-
 for _ in range(n):
     graph.append(list(map(int, input().split())))
-    
-ans = []
+
 def bfs():
-    visited[0][0] = 1
     q = deque()
     q.append((0, 0))
+    visited[0][0] = 1
     cnt = 0
-    
     while q:
         x, y = q.popleft()
-        
+
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
-            
+
             if 0 <= nx < n and 0 <= ny < m:
                 if visited[nx][ny] == 0:
                     if graph[nx][ny] == 0:
                         visited[nx][ny] = 1
                         q.append((nx, ny))
-                    elif graph[nx][ny] == 1:
+                    if graph[nx][ny] == 1:
                         visited[nx][ny] = 1
                         graph[nx][ny] = 0
                         cnt += 1
-    ans.append(cnt)
+    res.append(cnt)
     return cnt
-    
-    
-
+res = []
 time = 0
 while True:
-    time += 1
     visited = [[0] * m for _ in range(n)]
-    cnt = bfs()
-    if cnt == 0:
+    time += 1
+    ans = bfs()
+    if ans == 0:
         break
 
 print(time - 1)
-print(ans[-2])
+print(res[-2])
+
+
