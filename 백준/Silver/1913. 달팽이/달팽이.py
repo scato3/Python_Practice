8bcce951293c5a2388329ff64fa09f4ba3da6898
Hyunsu Shin <58941022+scato3@users.noreply.github.com>
2022-05-29 11:10:43 +0900

    [Silver IV] Title: 달팽이, Time: 332 ms, Memory: 124760 KB -BaekjoonHub

--- 백준/Silver/1913. 달팽이/달팽이.py
+++ 백준/Silver/1913. 달팽이/달팽이.py
@@ -1,59 +1,52 @@
 n = int(input())
 k = int(input())
-
 board = [[0] * n for _ in range(n)]
-
-r = n // 2
-c = n // 2
 value = 1
-dir = 1
-count = 1
-board[r][c] = value # 초기값
- 
+dir = 0
+x = n // 2
+y = n // 2
+board[x][y] = value
+cnt = 1
+
 while value != n * n:
-    if dir == 1:
-        for _ in range(count):
-            r -= 1
+    if dir == 0:
+        for i in range(cnt):
+            x -= 1
             value += 1
-            board[r][c] = value
-            
+            board[x][y] = value
             if value == n * n:
                 break
-        
-        dir = 2
-    elif dir == 2:
-        for _ in range(count):
-            c += 1
-            value += 1
-            board[r][c] = value
-            
-        count += 1
-        dir = 3
-    
-    elif dir == 3:
-        for _ in range(count):
-            r += 1
-            value += 1
-            board[r][c] = value
-        
-        dir = 4
-    elif dir == 4:
-        for _ in range(count):
-            c -= 1
-            value += 1
-            board[r][c] = value
-        
-        count += 1
         dir = 1
+    elif dir == 1:
+        for i in range(cnt):
+            y += 1
+            value += 1
+            board[x][y] = value
+        dir = 2
+        cnt += 1
+    elif dir == 2:
+        for i in range(cnt):
+            x += 1
+            value += 1
+            board[x][y] = value
+        dir = 3
+    elif dir == 3:
+        for i in range(cnt):
+            y -= 1
+            value += 1
+            board[x][y] = value
+        dir = 0
+        cnt += 1
 
-ans = []
+for i in board:
+    print(*i)
+
 for i in range(n):
     for j in range(n):
-        print(board[i][j], end = ' ')
         if board[i][j] == k:
-            ans.append(i + 1)
-            ans.append(j + 1)
-    
-    print()
+            print(i+1, j+1)
 
-print(*ans)
+
+
+
+
