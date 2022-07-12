394e80b7b2bf0a52b00f92d1f51c6f78f911a8d9
Hyunsu Shin <58941022+scato3@users.noreply.github.com>
2022-07-12 13:03:44 +0900

    [Bronze II] Title: 다이얼, Time: 76 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/5622. 다이얼/다이얼.py
+++ 백준/Bronze/5622. 다이얼/다이얼.py
@@ -1,11 +1,8 @@
-dial = ['ABC', 'DEF', 'GHI', 'JKL', 'MNO', 'PQRS', 'TUV', 'WXYZ']
-
+arr = ['ABC', 'DEF', 'GHI', 'JKL', 'MNO', 'PQRS', 'TUV', 'WXYZ']
 a = input()
-result = 0
-
-for i in a:
-    for j in dial:
-        if i in j:
-            result += dial.index(j) + 3
-
-print(result)
+tmp = 0
+for i in range(len(a)):
+    for j in arr:
+        if a[i] in j:
+            tmp += arr.index(j) + 3
+print(tmp)
