606e9d990da69362687a2c7898badfbbb34413ae
Hyunsu Shin <58941022+scato3@users.noreply.github.com>
2022-07-12 12:23:58 +0900

    [Bronze II] Title: 나머지, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.py
+++ 백준/Bronze/3052. 나머지/나머지.py
@@ -1,6 +1,13 @@
-arr = set()
+num = [0] * 42
 
-for _ in range(10):
-    arr.add(int(input()) % 42)
-    
-print(len(arr))
+for i in range(10):
+    a = int(input())
+    tmp = a % 42
+    num[tmp] += 1
+
+ans = 0
+for i in num:
+    if i > 0:
+        ans += 1
+
+print(ans)
