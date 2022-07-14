17abaf3de2806c33ee130f61d6bd511a745251ab
Hyunsu Shin <58941022+scato3@users.noreply.github.com>
2022-07-14 12:15:43 +0900

    [Silver V] Title: 셀프 넘버, Time: 76 ms, Memory: 31352 KB -BaekjoonHub

--- 백준/Silver/4673. 셀프 넘버/셀프 넘버.py
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.py
@@ -1,8 +1,12 @@
-arr = set()
-for i in range(1, 10001):
-    i = i + sum(map(int, str(i)))
-    arr.add(i)
+num = set(range(1, 10001))
+tmp_num = set()
 
 for i in range(1, 10001):
-    if i not in arr:
-        print(i)
+    for j in str(i):
+        i += int(j)
+    tmp_num.add(i)
+
+self_num = sorted(num - tmp_num)
+
+for i in self_num:
+    print(i)
