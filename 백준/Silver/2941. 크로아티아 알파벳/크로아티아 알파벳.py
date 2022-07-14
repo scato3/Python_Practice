ad173977b6fbbf44cb821807f72db6dbf843d016
Hyunsu Shin <58941022+scato3@users.noreply.github.com>
2022-07-14 14:16:10 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.py
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.py
@@ -1,8 +1,7 @@
-alpha = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z=']
+arr = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z=']
 
 a = input()
 
-for i in alpha:
+for i in arr:
     a = a.replace(i, '*')
-
 print(len(a))
