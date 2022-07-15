bd51937f4e1601017de3956dd3a930882768df3d
Hyunsu Shin <58941022+scato3@users.noreply.github.com>
2022-07-15 11:43:20 +0900

    [Silver V] Title: 소수, Time: 432 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/2581. 소수/소수.py
+++ 백준/Silver/2581. 소수/소수.py
@@ -1,21 +1,21 @@
-m = int(input())
-n = int(input())
-
-def isPrime(x):
-    for i in range(2, x):
-        if x % i == 0:
-            return 0
+def isPrime(n):
+    if n != 1:
+        for i in range(2, n):
+            if n % i == 0:
+                return 0
+    else:
+        return 0
     return 1
 
-ans = []
+m = int(input())
+n = int(input())
+arr = []
 for i in range(m, n+1):
-    if i <= 1:
-        continue
     if isPrime(i) == 1:
-        ans.append(i)
+        arr.append(i)
 
-if len(ans) == 0:
+if len(arr) == 0:
     print(-1)
 else:
-    print(sum(ans))
-    print(min(ans))
+    print(sum(arr))
+    print(min(arr))
