d4589bdf0c0a3c97e3d54c69c46310a9a5bb9dce
Hyunsu Shin <58941022+scato3@users.noreply.github.com>
2022-07-15 12:03:58 +0900

    [Silver V] Title: 나이순 정렬, Time: 4244 ms, Memory: 45468 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
@@ -1,11 +1,11 @@
 n = int(input())
 arr = []
-for _ in range(n):
-    age, name = map(str, input().split())
-    age = int(age)
-    arr.append((age, name))
-    
-arr.sort(key = lambda x : x[0])
 
+for i in range(n):
+    a, b = input().split()
+    a = int(a)
+    arr.append((a, b))
+
+arr.sort(key=lambda x:(x[0]))
 for i in arr:
-    print(*i)
+    print(i[0], i[1])
