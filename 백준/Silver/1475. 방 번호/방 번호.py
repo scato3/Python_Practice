fc7c6368cd7769208fd31adbaf8d124778c18211
Hyunsu Shin <58941022+scato3@users.noreply.github.com>
2022-07-18 12:31:32 +0900

    [Silver V] Title: 방 번호, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/1475. 방 번호/방 번호.py
+++ 백준/Silver/1475. 방 번호/방 번호.py
@@ -1,17 +1,13 @@
+arr = [0] * 10
 n = input()
-card = [0] * 10
-card6 = 0
-for i in n:
-    if i == '6' or i == '9':
-        card6 += 1
+
+for i in range(len(n)):
+    num = int(n[i])
+    if num == 6 or num == 9:
+        if arr[6] <= arr[9]:
+            arr[6] += 1
+        else:
+            arr[9] += 1
     else:
-        card[int(i)] += 1
-
-if card6 % 2 == 0:
-    card6 = card6 // 2
-else:
-    card6 = card6 // 2 + 1
-    
-card[6] = card6
-
-print(max(card))
+        arr[num] += 1
+print(max(arr))
