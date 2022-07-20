907af232d3987a8ce7cc364e12b07d6265bf8b59
Hyunsu Shin <58941022+scato3@users.noreply.github.com>
2022-07-20 16:34:02 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 4212 ms, Memory: 32420 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
@@ -1,16 +1,20 @@
-a, b = map(int, input().split())
-arr = [i for i in range(1, a+1)]
+from collections import deque
+n, k = map(int, input().split())
+arr = deque()
+for i in range(1, n+1):
+    arr.append(i)
+ans = []
+print('<',end='')
+while True:
+    if not arr:
+        break
+    for i in range(k-1):
+        tmp = arr.popleft()
+        arr.append(tmp)
+    ans.append(arr.popleft())
+print(', '.join(str(i) for i in ans), '>',sep='')
 
-num = b - 1
-answer = []
-for i in range(a):
-    if len(arr) > num:
-        answer.append(arr.pop(num))
-        num += b - 1
-    elif len(arr) <= num:
-        num = num % len(arr)
-        answer.append(arr.pop(num))
-        num += b - 1
-        
-print('<', ', '.join(str(i) for i in answer), '>', sep= '')
-    
+
+
+
+
