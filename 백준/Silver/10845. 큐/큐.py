2b45a3a46af1e9a72117135fa7be25149df7898a
Hyunsu Shin <58941022+scato3@users.noreply.github.com>
2022-07-20 11:38:02 +0900

    [Silver IV] Title: 큐, Time: 76 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/10845. 큐/큐.py
+++ 백준/Silver/10845. 큐/큐.py
@@ -1,32 +1,30 @@
-from collections import deque
-import sys
-n = int(sys.stdin.readline())
-arr = deque()
+from sys import stdin
 
-
-for _ in range(n):
-    a = sys.stdin.readline().split()
-    if a[0] == 'push':
-        arr.append(a[1])
-    elif a[0] == 'pop':
-        if arr:
-            print(arr.popleft())
-        else:
+n = int(stdin.readline())
+stack = []
+for i in range(n):
+    cmd = stdin.readline().split()
+    if cmd[0] == 'push':
+        stack.append(cmd[1])
+    if cmd[0] == 'pop':
+        if len(stack) == 0:
             print(-1)
-    elif a[0] == 'size':
-        print(len(arr))
-    elif a[0] == 'empty':
-        if arr:
-            print(0)
         else:
+            print(stack.pop(0))
+    if cmd[0] == 'size':
+        print(len(stack))
+    if cmd[0] == 'empty':
+        if len(stack) == 0:
             print(1)
-    elif a[0] == 'front':
-        if arr:
-            print(arr[0])
         else:
+            print(0)
+    if cmd[0] == 'front':
+        if len(stack) == 0:
             print(-1)
-    elif a[0] == 'back':
-        if arr:
-            print(arr[-1])
         else:
+            print(stack[0])
+    if cmd[0] == 'back':
+        if len(stack) == 0:
             print(-1)
+        else:
+            print(stack[-1])
