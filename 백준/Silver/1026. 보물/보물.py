7e4e2d6260ebd55d60e3759db1cf6d0b9249eff4
Hyunsu Shin <58941022+scato3@users.noreply.github.com>
2022-07-20 17:00:35 +0900

    [Silver IV] Title: 보물, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/1026. 보물/보물.py
+++ 백준/Silver/1026. 보물/보물.py
@@ -1,10 +1,12 @@
-t = int(input())
-arr1 = list(map(int, input().split()))
-arr2 = list(map(int, input().split()))
+n = int(input())
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
 
-sum = 0
-for i in range(t):
-    sum += max(arr2) * min(arr1)
-    arr1.remove(min(arr1))
-    arr2.remove(max(arr2))
-print(sum)
+a.sort()
+ans = 0
+for i in range(n):
+    x = a[i]
+    y = b.pop(b.index(max(b)))
+    ans += x * y
+
+print(ans)
