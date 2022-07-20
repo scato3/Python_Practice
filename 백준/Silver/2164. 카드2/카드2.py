f224e67d8fb0093d3a3b22ff78981391f9ec0a1d
Hyunsu Shin <58941022+scato3@users.noreply.github.com>
2022-07-20 16:43:28 +0900

    [Silver IV] Title: 카드2, Time: 264 ms, Memory: 49320 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.py
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -3,13 +3,8 @@ n = int(input())
 arr = deque()
 for i in range(1, n+1):
     arr.append(i)
+while len(arr) != 1:
+    arr.popleft()
+    arr.append(arr.popleft())
 
-while True:
-    if len(arr) == 1:
-        print(*arr)
-        break
-    else:
-        arr.popleft()
-        x = arr.popleft()
-        arr.append(x)
-
+print(*arr)
