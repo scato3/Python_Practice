bfa540c2d06a4fac8e786c6f13112fe30db37c79
Hyunsu Shin <58941022+scato3@users.noreply.github.com>
2022-07-21 12:10:56 +0900

    [Silver IV] Title: 듣보잡, Time: 4012 ms, Memory: 42108 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.py
+++ 백준/Silver/1764. 듣보잡/듣보잡.py
@@ -1,13 +1,15 @@
-n, m = map(int, input().split())
-a = set()
-for _ in range(n):
-    a.add(input())
-b = set()
-for _ in range(m):
-    b.add(input())
-
-res = sorted(list(a & b))
-
-print(len(res))
-for i in res:
+a, b = map(int, input().split())
+arr1 = set()
+arr2 = set()
+ans = []
+for i in range(a):
+    arr1.add(input())
+for i in range(b):
+    arr2.add(input())
+for i in arr1:
+    if i in arr2:
+        ans.append(i)
+print(len(ans))
+ans.sort()
+for i in ans:
     print(i)
