17a6639bbe656eae23e321a7f8b95fedea4c76eb
Hyunsu Shin <58941022+scato3@users.noreply.github.com>
2022-07-25 15:00:03 +0900

    [Silver IV] Title: 큐 2, Time: 1880 ms, Memory: 173440 KB -BaekjoonHub

---
+++ 백준/Silver/18258. 큐 2/큐 2.py
@@ -0,0 +1,30 @@
+from collections import deque
+import sys
+n = int(sys.stdin.readline())
+q = deque()
+for _ in range(n):
+    com = sys.stdin.readline().split()
+    if com[0] == 'push':
+        q.append(com[1])
+    elif com[0] == 'pop':
+        if len(q) == 0:
+            print(-1)
+        else:
+            print(q.popleft())
+    elif com[0] == 'size':
+        print(len(q))
+    elif com[0] == 'empty':
+        if len(q) == 0:
+            print(1)
+        else:
+            print(0)
+    elif com[0] == 'front':
+        if len(q) == 0:
+            print(-1)
+        else:
+            print(q[0])
+    elif com[0] == 'back':
+        if len(q) == 0:
+            print(-1)
+        else:
+            print(q[-1])
