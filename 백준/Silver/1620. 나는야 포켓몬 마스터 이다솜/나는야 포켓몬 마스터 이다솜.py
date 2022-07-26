5b40ff3ea2eba0ebb0d43e9c2751a909c0b09ec3
Hyunsu Shin <58941022+scato3@users.noreply.github.com>
2022-07-26 10:01:39 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 268 ms, Memory: 55420 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
@@ -1,15 +1,18 @@
+import sys
+
+input = sys.stdin.readline
+
 n, m = map(int, input().split())
-dic = {}
-arr = []
+dict = {}
 
 for i in range(1, n+1):
-    a = input()
-    dic[i] = a
-    dic[a] = i
+    a = input().rstrip()
+    dict[i] = a
+    dict[a] = i
 
 for i in range(m):
-    k = input()
-    if k.isdigit():
-        print(dic[int(k)])
+    ans = input().rstrip()
+    if ans.isdigit():
+        print(dict[int(ans)])
     else:
-        print(dic[k])
+        print(dict[ans])
