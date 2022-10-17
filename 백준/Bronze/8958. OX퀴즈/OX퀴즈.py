3e2b727704cd215c1a64980341e7c0cb81973bb7
Hyunsu Shin <58941022+scato3@users.noreply.github.com>
2022-10-17 13:35:00 +0900

    [Bronze II] Title: OX퀴즈, Time: 80 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/8958. OX퀴즈/OX퀴즈.py
+++ 백준/Bronze/8958. OX퀴즈/OX퀴즈.py
@@ -1,17 +1,14 @@
 n = int(input())
 
-for i in range(n):
-    OX = input()
-    lst = list(OX)
-    tmp = 0
+for _ in range(n):
+    k = input()
+    cnt = 0
     ans = 0
-
-    for k in lst:
-        if k == 'O':
-            tmp += 1
-            ans += tmp
-        else:
-            tmp = 0
+    for i in range(len(k)):
+        if k[i] == 'O':
+            cnt += 1
+            ans += cnt
+        if k[i] == 'X':
+            cnt = 0
     print(ans)
-
 
