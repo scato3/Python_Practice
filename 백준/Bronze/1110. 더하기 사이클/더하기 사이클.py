71fed4703cbfaa3c301fdcc4cf8dae83512d700d
Hyunsu Shin <58941022+scato3@users.noreply.github.com>
2022-10-17 12:59:50 +0900

    [Bronze I] Title: 더하기 사이클, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/1110. 더하기 사이클/더하기 사이클.py
+++ 백준/Bronze/1110. 더하기 사이클/더하기 사이클.py
@@ -1,14 +1,14 @@
 n = int(input())
 num = n
-cnt = 0
+k = 0
 
 while True:
     a = num // 10
     b = num % 10
     c = (a + b) % 10
     num = (b * 10) + c
-    cnt += 1
-
+    k += 1
     if num == n:
         break
-print(cnt)
+
+print(k)
