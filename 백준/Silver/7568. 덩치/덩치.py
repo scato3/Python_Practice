5b4a1535d6f67f485ecbbf56e16a4d62f22d4f6d
Hyunsu Shin <58941022+scato3@users.noreply.github.com>
2022-10-18 21:09:44 +0900

    [Silver V] Title: 덩치, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/7568. 덩치/덩치.py
+++ 백준/Silver/7568. 덩치/덩치.py
@@ -1,12 +1,13 @@
 n = int(input())
 arr = []
-for i in range(n):
-    a, b = map(int, input().split())
-    arr.append((a, b))
+
+for _ in range(n):
+    w, h = map(int, input().split())
+    arr.append((w, h))
 
 for i in arr:
-    ans = 1
-    for k in arr:
-        if i[0] < k[0] and i[1] < k[1]:
-            ans += 1
-    print(ans,end=' ')
+    rank = 1
+    for j in arr:
+        if i[0] < j[0] and i[1] < j[1]:
+            rank += 1
+    print(rank, end=' ')
