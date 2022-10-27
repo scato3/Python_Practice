ad53338c7ea72a51d3492868371ad57d2ce8482b
Hyunsu Shin <58941022+scato3@users.noreply.github.com>
2022-10-27 23:17:53 +0900

    [Silver III] Title: iSharp, Time: 116 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Silver/3568. iSharp/iSharp.py
@@ -0,0 +1,28 @@
+string = input()
+
+str_list = string.split(' ')
+
+basic = str_list[0]
+del str_list[0]
+
+for k in str_list:
+    k = k.replace(',', '').replace(';', '')
+
+    print(basic,end='')
+
+    for i in range(len(k) -1, 0, -1):
+        if not k[i].isalpha():
+            if k[i] == ']':
+                print('[',end='')
+            elif k[i] == '[':
+                print(']',end='')
+            else:
+                print(k[i],end='')
+
+    print(' ',end='')
+
+    for i in range(len(k)):
+        if k[i].isalpha():
+            print(k[i],end='')
+
+    print(';')
