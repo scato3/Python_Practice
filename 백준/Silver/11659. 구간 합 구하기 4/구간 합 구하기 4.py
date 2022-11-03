1b93eef94ef130a07bdbef7d44b049957ed471f1
Hyunsu Shin <58941022+scato3@users.noreply.github.com>
2022-11-03 16:40:18 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 4792 ms, Memory: 128328 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.py
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.py
@@ -1,12 +1,13 @@
 n, m = map(int, input().split())
 arr = list(map(int, input().split()))
-prefix = [0]
+prefix_sum = [0]
 
-tmp = 0
+temp = 0
+
 for i in arr:
-    tmp += i
-    prefix.append(tmp)
+    temp += i
+    prefix_sum.append(temp)
 
 for i in range(m):
     a, b = map(int, input().split())
-    print(prefix[b] - prefix[a-1])
+    print(prefix_sum[b] - prefix_sum[a-1])
