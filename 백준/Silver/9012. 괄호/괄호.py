ab9c356d79371279e884f9a75060520de5591aa3
Hyunsu Shin <58941022+scato3@users.noreply.github.com>
2022-11-03 11:12:07 +0900

    [Silver IV] Title: 괄호, Time: 132 ms, Memory: 115004 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.py
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -1,17 +1,18 @@
 n = int(input())
+
 for _ in range(n):
     a = input()
     s = list(a)
-    cnt = 0
+    tmp = 0
     for i in s:
         if i == '(':
-            cnt += 1
-        elif i == ')':
-            cnt -= 1
-        if cnt < 0:
+            tmp += 1
+        if i == ')':
+            tmp -= 1
+        if tmp < 0:
             print('NO')
             break
-    if cnt > 0:
+    if tmp > 0:
         print('NO')
-    if cnt == 0:
+    if tmp == 0:
         print('YES')
