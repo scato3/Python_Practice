1ca71f0cf7e784f14ff19aa7353d3d3d27fbb283
Hyunsu Shin <58941022+scato3@users.noreply.github.com>
2022-11-03 14:03:09 +0900

    [Silver IV] Title: 30, Time: 168 ms, Memory: 120584 KB -BaekjoonHub

--- 백준/Silver/10610. 30/30.py
+++ 백준/Silver/10610. 30/30.py
@@ -1,10 +1,12 @@
 n = list(input())
 n.sort(reverse=True)
-sum = 0
-for i in n:
-    sum += int(i)
+tmp = 0
 
-if sum % 3 != 0 or '0' not in n:
+for i in n:
+    tmp += int(i)
+
+if tmp % 3 != 0 or '0' not in n:
     print(-1)
 else:
-    print(''.join(n))
+    for i in n:
+        print(i, end='')
