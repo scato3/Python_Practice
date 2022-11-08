66dc4deb24e1e0b4ce1af21f6b441f1a0fb37bba
Hyunsu Shin <58941022+scato3@users.noreply.github.com>
2022-11-08 15:08:32 +0900

    [Silver IV] Title: 스택, Time: 164 ms, Memory: 116440 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.py
+++ 백준/Silver/10828. 스택/스택.py
@@ -1,26 +1,26 @@
 import sys
-n = int(sys.stdin.readline())
 
-stack = []
+n = int(input())
+arr = []
+
 for i in range(n):
     com = sys.stdin.readline().split()
     if com[0] == 'push':
-        stack.append(com[1])
+        arr.append(com[1])
     elif com[0] == 'pop':
-        if len(stack) == 0:
+        if len(arr) == 0:
             print(-1)
         else:
-            print(stack.pop())
+            print(arr.pop())
     elif com[0] == 'size':
-        print(len(stack))
+        print(len(arr))
     elif com[0] == 'empty':
-        if len(stack) == 0:
+        if len(arr) == 0:
             print(1)
         else:
             print(0)
     elif com[0] == 'top':
-        if len(stack) == 0:
+        if len(arr) == 0:
             print(-1)
         else:
-            print(stack[-1])
-
+            print(arr[-1])
