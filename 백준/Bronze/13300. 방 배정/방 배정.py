b8c120c4f116af5be55712e9d19f1ac68fbf9aec
Hyunsu Shin <58941022+scato3@users.noreply.github.com>
2022-11-09 13:27:15 +0900

    [Bronze II] Title: 방 배정, Time: 132 ms, Memory: 114328 KB -BaekjoonHub

--- 백준/Bronze/13300. 방 배정/방 배정.py
+++ 백준/Bronze/13300. 방 배정/방 배정.py
@@ -1,24 +1,26 @@
 n, k = map(int, input().split())
+female = [0] * 7
+male = [0] * 7
 
-boy = [0] * 1000
-girl = [0] * 1000
-
-for i in range(n):
-    s, age = map(int, input().split())
-    
+for _ in range(n):
+    s, y = map(int, input().split())
     if s == 0:
-        girl[age] += 1
+        female[y] += 1
     else:
-        boy[age] += 1
+        male[y] += 1
 
-for i in range(1, 7):
-    if girl[i] % k == 0:
-        girl[i] = girl[i] // k
-    else:
-        girl[i] = (girl[i] // k) + 1
-    if boy[i] % k == 0: 
-        boy[i] = boy[i] // k
-    else:
-        boy[i] = (boy[i] // k) + 1
+ans = 0
 
-print(sum(girl) + sum(boy))
+for i in female:
+    if i % k == 0:
+        ans += i // k
+    else:
+        ans += (i // k) + 1
+
+for i in male:
+    if i % k == 0:
+        ans += i // k
+    else:
+        ans += (i // k) + 1
+
+print(ans)
