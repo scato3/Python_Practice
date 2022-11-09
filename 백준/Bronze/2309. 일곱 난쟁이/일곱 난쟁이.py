9d67dbc8287b50c3071e1d6a24316e7d8cdc3926
Hyunsu Shin <58941022+scato3@users.noreply.github.com>
2022-11-09 14:16:52 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 108 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
@@ -1,13 +1,18 @@
 arr = []
-for i in range(9):
+
+for _ in range(9):
     arr.append(int(input()))
 
 arr.sort()
-for i in range(9):
-    for j in range(i+1, 9):
+
+temp1, temp2 = 0, 0
+
+for i in range(len(arr)):
+    for j in range(i+1, len(arr)):
         if sum(arr) - arr[i] - arr[j] == 100:
             temp1 = arr[i]
             temp2 = arr[j]
+
 arr.remove(temp1)
 arr.remove(temp2)
 
