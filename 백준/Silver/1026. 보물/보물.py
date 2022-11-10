55af0878c8840b4430ccb8d7efc6385d44a6748b
Hyunsu Shin <58941022+scato3@users.noreply.github.com>
2022-11-10 11:50:24 +0900

    [Silver IV] Title: 보물, Time: 116 ms, Memory: 114328 KB -BaekjoonHub

--- 백준/Silver/1026. 보물/보물.py
+++ 백준/Silver/1026. 보물/보물.py
@@ -1,12 +1,11 @@
 n = int(input())
 a = list(map(int, input().split()))
 b = list(map(int, input().split()))
-
 a.sort()
 ans = 0
-for i in range(n):
-    x = a[i]
-    y = b.pop(b.index(max(b)))
-    ans += x * y
 
+for i in range(n):
+    ans += min(a) * max(b)
+    a.pop(a.index(min(a)))
+    b.pop(b.index(max(b)))
 print(ans)
