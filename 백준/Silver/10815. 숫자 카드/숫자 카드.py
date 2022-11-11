fbc9b9d6da72ebb20212bc4a82ad6f90322187e3
Hyunsu Shin <58941022+scato3@users.noreply.github.com>
2022-11-11 16:46:33 +0900

    [Silver V] Title: 숫자 카드, Time: 608 ms, Memory: 253048 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.py
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -1,11 +1,10 @@
 n = int(input())
 arr1 = set(map(int, input().split()))
-num = int(input())
+m = int(input())
 arr2 = list(map(int, input().split()))
-ans = []
+
 for i in arr2:
     if i in arr1:
-        ans.append(1)
+        print(1,end=' ')
     else:
-        ans.append(0)
-print(*ans)
+        print(0,end=' ')
