ff1f64cf92abd2aad29dddf941efdf7ed33a61bc
Hyunsu Shin <58941022+scato3@users.noreply.github.com>
2022-11-19 17:35:53 +0900

    [Silver IV] Title: 덱, Time: 208 ms, Memory: 118304 KB -BaekjoonHub

--- 백준/Silver/10866. 덱/덱.py
+++ 백준/Silver/10866. 덱/덱.py
@@ -1,39 +1,41 @@
 from collections import deque
 import sys
 
-n = int(input())
-arr = deque()
-for i in range(n):
+arr = deque([])
+
+for _ in range(int(input())):
     com = sys.stdin.readline().split()
     if com[0] == 'push_front':
         arr.appendleft(com[1])
-    elif com[0] == 'push_back':
+    if com[0] == 'push_back':
         arr.append(com[1])
-    elif com[0] == 'pop_front':
+    if com[0] == 'pop_front':
         if len(arr) == 0:
             print(-1)
         else:
             print(arr.popleft())
-    elif com[0] == 'pop_back':
+    if com[0] == 'pop_back':
         if len(arr) == 0:
             print(-1)
         else:
             print(arr.pop())
-    elif com[0] == 'size':
+    if com[0] == 'size':
         print(len(arr))
-    elif com[0] == 'empty':
+    if com[0] == 'empty':
         if len(arr) == 0:
             print(1)
         else:
             print(0)
-    elif com[0] == 'front':
-        if len(arr) == 0:
+    if com[0] == 'front':
+        if(len(arr)) == 0:
             print(-1)
         else:
             print(arr[0])
-    elif com[0] == 'back':
-        if len(arr) == 0:
+    if com[0] == 'back':
+        if(len(arr)) == 0:
             print(-1)
         else:
             print(arr[-1])
+
+
 
