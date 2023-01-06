47d3842abce06da35433747db6abbcf885dd634c
Hyunsu Shin <58941022+scato3@users.noreply.github.com>
2023-01-06 17:33:59 +0900

    [Silver V] Title: 뒤집기, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/1439. 뒤집기/뒤집기.py
+++ 백준/Silver/1439. 뒤집기/뒤집기.py
@@ -1,8 +1,12 @@
-s = input()
+# 백준 1439 뒤집기 SILVER V
+# https://www.acmicpc.net/problem/1439
+
+n = input()
 cnt = 0
 
-for i in range(len(s) - 1):
-    if s[i] != s[i+1]:
+for i in range(len(n) - 1):
+    if n[i] != n[i+1]:
         cnt += 1
+print((cnt + 1)// 2)
 
-print((cnt+1) // 2)
+
