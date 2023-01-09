e0e358d7c3e29c3563c4fdeb63672cb7ff1afc77
Hyunsu Shin <58941022+scato3@users.noreply.github.com>
2023-01-09 12:33:31 +0900

    [Silver III] Title: 팰린드롬 만들기, Time: 36 ms, Memory: 30748 KB -BaekjoonHub

--- 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.py
+++ 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.py
@@ -1,20 +1,26 @@
-name = input()
-cnt = [0 for _ in range(26)]
+# 백준 1213 팰린드롬 만들기 SILVER lll
+# https://www.acmicpc.net/problem/1213
 
-for i in name:
-    cnt[ord(i) - 65] += 1
+alpha =[0 for _ in range(26)]
+k = input()
 
-odd = 0
-oddName = ''
-alpha = ''
+for i in k:
+    alpha[ord(i) - 65] += 1
 
+cnt = 0
+odd_alpha = ''
+pha = ''
 for i in range(26):
-    if cnt[i] % 2 == 1:
-        odd += 1
-        oddName += chr(i+65)
-    alpha += chr(i+65) * (cnt[i] // 2)
-    
-if odd > 1:
+    if alpha[i] % 2 == 1:
+        cnt += 1
+        odd_alpha += chr(i+65)
+    pha += chr(i+65) * (alpha[i] // 2)
+
+if cnt > 1:
     print("I'm Sorry Hansoo")
 else:
-    print(alpha + oddName + alpha[::-1])
+    print(pha + odd_alpha + pha[::-1])
+
+
+
+
