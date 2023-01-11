321b456ff7343cb8f791d2ea8213effb5d769ac7
Hyunsu Shin <58941022+scato3@users.noreply.github.com>
2023-01-11 13:41:36 +0900

    [Silver I] Title: 미로 탐색, Time: 72 ms, Memory: 34112 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,26 +1,30 @@
+# 백준 2178 미로 탐색 SILVER l
+# https://www.acmicpc.net/problem/2178
+
+from collections import deque
+
 n, m = map(int, input().split())
-board = []
+graph = []
 for _ in range(n):
-    board.append(list(map(int, input()))) # 미로 만들기
-    
-visited = [[0] * m for _ in range(n)] # 방문했는지 확인
-dx = [1, 0, -1, 0]
-dy = [0, 1, 0, -1]
+    graph.append(list(map(int, input())))
 
-queue = [(0, 0)]
-visited[0][0] = 1
+dx = [-1, 1, 0, 0] # 상하좌우
+dy = [0, 0, -1, 1]
 
-while queue:
-    x, y = queue.pop(0)
-    
-    if x == n - 1 and y == m -1:
-        print(visited[x][y])
-        break
-    for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
-        if 0 <= nx < n and 0 <= ny < m:
-            if visited[nx][ny] == 0 and board[nx][ny] == 1:
-                visited[nx][ny] = visited[x][y] + 1
-                queue.append((nx, ny))
+def bfs(x, y):
+    q = deque()
+    q.append((x, y))
+    while q:
+        x, y = q.popleft()
 
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if 0 <= nx < n and 0 <= ny < m and graph[nx][ny] == 1:
+                q.append((nx, ny))
+                graph[nx][ny] = graph[x][y] + 1
+
+    return graph[n-1][m-1]
+
+print(bfs(0, 0))
