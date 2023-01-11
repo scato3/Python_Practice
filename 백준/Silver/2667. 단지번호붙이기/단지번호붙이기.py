19e174619ea3a9152915b307d3f4c2286607fb6a
Hyunsu Shin <58941022+scato3@users.noreply.github.com>
2023-01-11 14:13:10 +0900

    [Silver I] Title: 단지번호붙이기, Time: 60 ms, Memory: 34132 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,36 +1,41 @@
+# 백준 2667 단지번호 붙이기 SILVER l
+# https://www.acmicpc.net/problem/2667
+
+from collections import deque
 n = int(input())
-board = []
+graph = []
 
 for _ in range(n):
-    board.append(list(map(int, input())))
+    graph.append(list(map(int, input())))
 
-dx = [1, 0, -1, 0]
-dy = [0, 1, 0, -1]
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 
-cnt = 0
-apt = []
-def dfs(x, y):
-    global cnt
-    board[x][y] = 0
-    cnt += 1
-    
-    for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
-        
-        if nx < 0 or nx >= n or ny < 0 or ny >= n:
-            continue
-        if board[nx][ny] == 1:
-            dfs(nx, ny)
-    
+def bfs(x, y):
+    q = deque()
+    q.append((x, y))
+    graph[x][y] = 0
+    cnt = 1
+
+    while q:
+        x, y = q.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < n and 0 <= ny < n and graph[nx][ny] == 1:
+                graph[nx][ny] = 0
+                q.append((nx, ny))
+                cnt += 1
+    return cnt
+
+tmp = []
+
 for i in range(n):
     for j in range(n):
-        if board[i][j] == 1:
-            cnt = 0
-            dfs(i, j)
-            apt.append(cnt)
+        if graph[i][j] == 1:
+            tmp.append(bfs(i, j))
+tmp.sort()
 
-print(len(apt))
-apt.sort()
-for i in apt:
+print(len(tmp))
+for i in tmp:
     print(i)
