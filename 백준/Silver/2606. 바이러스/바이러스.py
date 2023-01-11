24466becedd2197dffdc1452859ce2f8523bbdb6
Hyunsu Shin <58941022+scato3@users.noreply.github.com>
2023-01-11 13:53:17 +0900

    [Silver III] Title: 바이러스, Time: 40 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,7 +1,10 @@
+# 백준 2606 바이러스 SILVER lll
+# https://www.acmicpc.net/problem/2606
+
 n = int(input())
 m = int(input())
-
 graph = [[] * n for _ in range(n+1)]
+
 for _ in range(m):
     a, b = map(int, input().split())
     graph[a].append(b)
@@ -10,12 +13,13 @@ for _ in range(m):
 cnt = 0
 visited = [0] * (n+1)
 
-def dfs(start):
+def dfs(v):
     global cnt
-    visited[start] = 1
-    for i in graph[start]:
+    visited[v] = 1
+    for i in graph[v]:
         if visited[i] == 0:
             dfs(i)
             cnt += 1
+
 dfs(1)
 print(cnt)
