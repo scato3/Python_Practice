3b33dcf4d0ee8b072a3049e999ae13c2bbf97664
Hyunsu Shin <58941022+scato3@users.noreply.github.com>
2023-01-12 10:52:24 +0900

    [Gold V] Title: 토마토, Time: 1436 ms, Memory: 98540 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -1,39 +1,51 @@
+# 백준 7576 토마토 GOLD V
+# https://www.acmicpc.net/problem/7576
+
 from collections import deque
 
 m, n = map(int, input().split())
+graph = []
+for _ in range(n):
+    graph.append(list(map(int, input().split())))
 
-board = []
-queue = deque([])
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+q = deque()
 
 for i in range(n):
-    board.append(list(map(int, input().split())))
     for j in range(m):
-        if board[i][j] == 1:
-            queue.append([i, j])
-
-dx = [1, 0, -1, 0]
-dy = [0, 1, 0, -1]
+        if graph[i][j] == 1:
+            q.append((i, j))
 
 def bfs():
-    while queue:
-        x, y = queue.popleft()
-        
+    while q:
+        x,y = q.popleft()
+
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
+
+            if 0 <= nx < n and 0 <= ny < m and graph[nx][ny] == 0:
+                q.append((nx, ny))
+                graph[nx][ny] = graph[x][y] + 1
+
+    return graph
+
+
+tmp = []
+for i in range(n):
+    for j in range(m):
+        if graph[i][j] == 1:
+            tmp = bfs()
             
-            if 0 <= nx < n and 0 <= ny < m and board[nx][ny] == 0:
-                queue.append([nx, ny])
-                board[nx][ny] = board[x][y] + 1
-
-bfs()
-res = 0
-
-for i in board:
+max_num = 0
+for i in tmp:
     for j in i:
         if j == 0:
             print(-1)
             exit(0)
-    res = max(res, max(i))
+    else:
+        max_num = max(max_num, max(i))
 
-print(res - 1)
+print(max_num - 1)
