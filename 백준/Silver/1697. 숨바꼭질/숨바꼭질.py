beded85d134e053c1e8c3d3b0155eb39fc9ea024
Hyunsu Shin <58941022+scato3@users.noreply.github.com>
2023-01-12 11:41:59 +0900

    [Silver I] Title: 숨바꼭질, Time: 100 ms, Memory: 35320 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -1,18 +1,27 @@
+# 백준 1697 숨바꼭질 SILVER l
+# https://www.acmicpc.net/problem/1697
+
 from collections import deque
-MAX = 10 ** 5
 
-def bfs(v):
-    queue = deque([v])
-    while queue:
-        v = queue.popleft()
-        if v == K:
-            return visited[v]
-        for i in (v-1, v+1, 2*v):
-            if 0 <= i <= MAX and not visited[i]:
-                visited[i] = visited[v] + 1
-                queue.append(i)
+MAX = 100001
+tmp = [0] * MAX
 
-N, K = map(int, input().split())
-visited = [0 for _ in range(MAX+1)]
-print(bfs(N))
+n, k = map(int, input().split())
+
+q = deque()
+q.append(n)
+
+def bfs():
+    while q:
+        x = q.popleft()
+        if x == k:
+            print(tmp[k])
+            break
+        else:
+            for nx in (x-1, x+1, x*2):
+                if 0 <= nx < MAX and tmp[nx] == 0:
+                    tmp[nx] = tmp[x] + 1
+                    q.append(nx)
+
+bfs()
 
