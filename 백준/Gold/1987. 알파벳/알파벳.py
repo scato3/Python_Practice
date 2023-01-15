14a6cb1c2685c4e0300d027b9dd1ad3a27882534
Hyunsu Shin <58941022+scato3@users.noreply.github.com>
2023-01-15 12:53:06 +0900

    [Gold IV] Title: 알파벳, Time: 7720 ms, Memory: 116168 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.py
+++ 백준/Gold/1987. 알파벳/알파벳.py
@@ -1,28 +1,28 @@
+r, c = map(int, input().split())
+graph = []
+
+for _ in range(r):
+    graph.append(list(input()))
+
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-r, c = map(int, input().split())
+answer = 1
 
-graph = []
-for _ in range(r):
-    graph.append(input())
-    
 def bfs(x, y):
+    global answer
     q = set([(x, y, graph[x][y])])
-    res = 1
-    
+
     while q:
-        x, y, visited = q.pop()
-        
+        x, y, tmp = q.pop()
+
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
-            
-            if 0 <= nx < r and 0 <= ny < c:
-                if graph[nx][ny] not in visited:
-                    next_visited = graph[nx][ny] + visited
-                    q.add((nx, ny, next_visited))
-                    res = max(res, len(next_visited))
-    return res
-    
-print(bfs(0, 0))
+
+            if 0 <= nx < r and 0 <= ny < c and graph[nx][ny] not in tmp:
+                q.add((nx, ny, tmp+graph[nx][ny]))
+                answer = max(answer, len(tmp) + 1)
+
+bfs(0, 0)
+print(answer)
