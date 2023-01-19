a7d7d0223870bf3cee17db652bfffc20aa0c07d5
Hyunsu Shin <58941022+scato3@users.noreply.github.com>
2023-01-19 14:24:53 +0900

    [Silver III] Title: 2×n 타일링, Time: 112 ms, Memory: 113248 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
@@ -1,6 +1,10 @@
-ans = [0, 1, 2]
-for i in range(3, 1001):
-    ans.append(ans[i-2] + ans[i-1])
+# 백준 11726 2xn 타일링 SILVER lll
+# https://www.acmicpc.net/problem/11726
 
 n = int(input())
-print(ans[n] % 10007)
+arr = [0, 1, 2]
+
+for i in range(3, 1002):
+    arr.append(arr[i-2] + arr[i-1])
+
+print(arr[n] % 10007)
