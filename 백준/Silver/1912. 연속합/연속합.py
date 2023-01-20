1ba2d4a1892baab96e8f38ab5c5658d9cb17b1a0
Hyunsu Shin <58941022+scato3@users.noreply.github.com>
2023-01-20 18:39:09 +0900

    [Silver II] Title: 연속합, Time: 132 ms, Memory: 128152 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.py
+++ 백준/Silver/1912. 연속합/연속합.py
@@ -1,9 +1,9 @@
 n = int(input())
 arr = list(map(int, input().split()))
 
-temp = [arr[0]]
+tmp = [arr[0]]
 
-for i in range(len(arr) - 1):
-    temp.append(max(temp[i] + arr[i+1], arr[i+1]))
+for i in range(n - 1):
+    tmp.append(max(tmp[i] + arr[i+1], arr[i+1]))
 
-print(max(temp))
+print(max(tmp))
