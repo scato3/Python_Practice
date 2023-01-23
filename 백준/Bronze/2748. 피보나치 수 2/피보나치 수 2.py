691361aef3fd16ef7c1812ee45441a67d34c55f6
Hyunsu Shin <58941022+scato3@users.noreply.github.com>
2023-01-23 11:38:13 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 112 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
@@ -1,14 +1,18 @@
+# 백준 2748 피보나치 수 2 BRONZE l
+# https://www.acmicpc.net/problem/2748
+
 n = int(input())
 
-fibo = []
+fib = []
 num = 0
+
 for i in range(n+1):
     if i == 0:
         num = 0
-    elif i <= 2:
+    elif i == 1 or n == 2:
         num = 1
     else:
-        num = fibo[-1] + fibo[-2]
-    fibo.append(num)
+        num = fib[i-1] + fib[i-2]
+    fib.append(num)
 
-print(fibo[-1])
+print(fib[-1])
