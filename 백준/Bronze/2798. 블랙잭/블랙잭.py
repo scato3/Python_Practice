09aa41aa5863e33b007cd9a4ce9e1ae51c5b0bae
Hyunsu Shin <58941022+scato3@users.noreply.github.com>
2023-02-02 10:53:05 +0900

    [Bronze II] Title: 블랙잭, Time: 132 ms, Memory: 125528 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.py
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -1,14 +1,13 @@
 n, m = map(int, input().split())
-arr = list(map(int, input().split()))
-
-res = 0
+lst = list(map(int, input().split()))
+arr = []
 
 for i in range(n):
     for j in range(i+1, n):
         for k in range(j+1, n):
-            if arr[i] + arr[j] + arr[k] > m:
+            if lst[i] + lst[j] + lst[k] > m:
                 continue
             else:
-                res = max(res, arr[i] + arr[j] + arr[k])
+                arr.append(lst[i] + lst[j] + lst[k])
 
-print(res)
+print(max(arr))
