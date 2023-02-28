64d54c1679842a26aae3dc6d8eec7adf5d26e8f3
Hyunsu Shin <58941022+scato3@users.noreply.github.com>
2023-02-28 11:34:42 +0900

    [Gold V] Title: 암호 만들기, Time: 116 ms, Memory: 114328 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.py
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.py
@@ -1,25 +1,14 @@
-def back(cnt, idx):
-    if cnt == l:
-        vo, co = 0, 0
+from itertools import combinations
 
-        for i in range(l):
-            if ans[i] in tmp:
-                vo += 1
-            else:
-                co += 1
+L, C = map(int, input().split())
+alpha = sorted(input().split())
+words = combinations(alpha, L)
 
-        if vo >= 1 and co >= 2:
-            print(''.join(ans))
-        return
+for word in words:
+    cnt_vow = 0
+    for i in word:
+        if i in "aeiou":
+            cnt_vow += 1
 
-    for i in range(idx, c):
-        ans.append(word[i])
-        back(cnt+1, i + 1)
-        ans.pop()
-
-l, c = map(int, input().split())
-tmp = ['a', 'e', 'i', 'o', 'u']
-word = sorted(list(map(str, input().split())))
-ans = []
-
-back(0, 0)
+    if cnt_vow >= 1 and L - cnt_vow >= 2:
+        print(''.join(word))
