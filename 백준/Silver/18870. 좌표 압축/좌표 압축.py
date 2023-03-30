dc4e4b826a66624299f77c449c35750f35fdc3bf
Hyunsu Shin <58941022+scato3@users.noreply.github.com>
2023-03-30 10:02:22 +0900

    [Silver II] Title: 좌표 압축, Time: 1864 ms, Memory: 154800 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.py
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.py
@@ -1,7 +1,8 @@
 n = int(input())
 arr = list(map(int, input().split()))
+arr2 = sorted(list(set(arr)))
 
-arr2 = sorted(set(arr))
-dic = {arr2[i] : i for i in range(len(arr2))} # key : value
+dic = {arr2[i] : i for i in range(len(arr2))}
+
 for i in arr:
     print(dic[i], end=' ')
