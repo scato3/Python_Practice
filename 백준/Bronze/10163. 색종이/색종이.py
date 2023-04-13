683f493f89c7dec2391bc4cfe6576b5905bcf3aa
Hyunsu Shin <58941022+scato3@users.noreply.github.com>
2023-04-13 18:21:52 +0900

    [Bronze I] Title: 색종이, Time: 180 ms, Memory: 39044 KB -BaekjoonHub

--- 백준/Bronze/10163. 색종이/색종이.py
+++ 백준/Bronze/10163. 색종이/색종이.py
@@ -1,17 +1,15 @@
+arr = [[0 for _ in range(1001)] for _ in range(1001)]
+
 n = int(input())
-board = [[0] * 1001 for _ in range(1001)]
 
 for k in range(1, n+1):
-    a, b, c, d = map(int, input().split())
-    for i in range(a, a+c):
-        for j in range(b, b+d):
-            board[i][j] = k
+    x, y, w, h = map(int, input().split())
+    for i in range(x, x+w):
+        for j in range(y, y+h):
+            arr[i][j] = k
 
-cnt = [0] * (n+1)
-
-for i in range(1001):
-    for j in range(1001):
-        cnt[board[i][j]] += 1
-
-for i in range(1, len(cnt)):
-    print(cnt[i])
+for i in range(1, n+1):
+    result = 0
+    for j in arr:
+        result += j.count(i)
+    print(result)
