7416ed17d09f5ee986825d4fea66b51a4685a396
Hyunsu Shin <58941022+scato3@users.noreply.github.com>
2023-04-13 18:08:53 +0900

    [Silver IV] Title: 색종이 - 2, Time: 56 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2567. 색종이 － 2/색종이 － 2.py
+++ 백준/Silver/2567. 색종이 － 2/색종이 － 2.py
@@ -1,30 +1,35 @@
-from collections import deque
+arr = [[0 for _ in range(101)]for _ in range(101)]
 
-graph = [[0] * 101 for _ in range(101)]
+n = int(input())
+
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
-n = int(input())
-for _ in range(n):
-    a, b = map(int, input().split())
-    for i in range(a, a+10):
-        for j in range(b, b+10):
-            graph[i][j] = 1
 
-res = 0
-for i in range(1, 101):
-    for j in range(1, 101):
-        if graph[i][j] == 1:
+for _ in range(n):
+    x, y = map(int, input().split())
+
+    for i in range(x, x+10):
+        for j in range(y, y+10):
+            arr[i][j] = 1
+
+result = 0
+for i in range(101):
+    for j in range(101):
+        if arr[i][j] == 1:
             cnt = 0
+
             for k in range(4):
                 nx = i + dx[k]
                 ny = j + dy[k]
 
-                if graph[nx][ny] == 1:
+                if arr[nx][ny] == 1:
                     cnt += 1
             if cnt == 3:
-                res += 1
+                result += 1
             if cnt == 2:
-                res += 2
+                result += 2
 
-print(res)
+print(result)
+
+
 
