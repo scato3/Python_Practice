cc137aee067f01ec91df6dc780d26c30886d85b4
Hyunsu Shin <58941022+scato3@users.noreply.github.com>
2023-04-13 17:45:26 +0900

    [Silver V] Title: 색종이, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.py
+++ 백준/Silver/2563. 색종이/색종이.py
@@ -1,13 +1,16 @@
+arr = [[0 for _ in range(101)] for _ in range(101)]
+
 n = int(input())
-arr = [[0] * 101 for _ in range(101)]
+
 for _ in range(n):
-    a, b = map(int, input().split())
-    for i in range(a, a+10):
-        for j in range(b, b+10):
+    x, y = map(int, input().split())
+
+    for i in range(x, x+10):
+        for j in range(y, y+10):
             arr[i][j] = 1
+result = 0
 
-ans = 0
 for i in arr:
-    ans += i.count(1)
+    result += i.count(1)
 
-print(ans)
+print(result)
