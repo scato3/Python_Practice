85bebec32da1d3169104d38178442cb21c619d62
Hyunsu Shin <58941022+scato3@users.noreply.github.com>
2023-04-18 12:07:46 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
@@ -1,11 +1,9 @@
 n = int(input())
-arr = []
 ans = 0
 
 for _ in range(n):
     word = input()
     error = 0
-
     for i in range(len(word) - 1):
         if word[i] != word[i+1]:
             tmp = word[i+1:]
@@ -13,6 +11,7 @@ for _ in range(n):
                 error += 1
     if error == 0:
         ans += 1
+
 
 print(ans)
 
