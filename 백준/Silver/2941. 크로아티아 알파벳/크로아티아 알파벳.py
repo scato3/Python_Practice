39444322fc103e6077c4e5c6a186ad23dc56d8ef
Hyunsu Shin <58941022+scato3@users.noreply.github.com>
2023-04-18 12:26:11 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.py
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.py
@@ -1,8 +1,8 @@
-k = input()
-
 cro = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z=']
 
-for i in cro:
-    k = k.replace(i, '!')
+a = input()
 
-print(len(k))
+for i in cro:
+    a = a.replace(i, '!')
+
+print(len(a))
