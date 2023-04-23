ea482a60fd548c4c86baa355e8a84a092cb4aeba
Hyunsu Shin <58941022+scato3@users.noreply.github.com>
2023-04-23 12:45:46 +0900

    [Silver IV] Title: 설탕 배달, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.py
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -1,15 +1,14 @@
-# 백준 설탕배달 SILVER lV
-# https://www.acmicpc.net/problem/2839
-
 n = int(input())
 cnt = 0
 
 while n >= 0:
     if n % 5 == 0:
         cnt += n // 5
+        n //= 5
         print(cnt)
         break
     n -= 3
     cnt += 1
 else:
     print(-1)
+
