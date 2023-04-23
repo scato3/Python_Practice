7bbaa888b9f38bc62692158ddcae143c9b9c1944
Hyunsu Shin <58941022+scato3@users.noreply.github.com>
2023-04-23 12:02:21 +0900

    [Silver IV] Title: 등수 구하기, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1205. 등수 구하기/등수 구하기.py
+++ 백준/Silver/1205. 등수 구하기/등수 구하기.py
@@ -1,18 +1,16 @@
-n, new, p = map(int, input().split())
+n, num, p = map(int, input().split())
 
 if n:
     score = list(map(int, input().split()))
-    score.append(new)
+    score.append(num)
     score.sort(reverse=True)
-    idx = score.index(new) + 1
+    idx = score.index(num) + 1
 
     if idx > p:
         print(-1)
+    elif n == p and score[-1] == num:
+        print(-1)
     else:
-        if n == p and new == score[-1]:
-            print(-1)
-        else:
-            print(idx)
-
+        print(idx)
 else:
     print(1)
