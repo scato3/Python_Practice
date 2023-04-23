5b205971bd9a356a8ab8d4f18b7d216970ede91e
Hyunsu Shin <58941022+scato3@users.noreply.github.com>
2023-04-23 12:52:49 +0900

    [Silver IV] Title: ATM, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.py
+++ 백준/Silver/11399. ATM/ATM.py
@@ -1,16 +1,8 @@
-# 백준 ATM SILVER lV
-# https://www.acmicpc.net/problem/11399
-
 n = int(input())
-arr = list(map(int, input().split()))
-arr.sort()
-ans = 0
+arr = sorted(list(map(int, input().split())))
+result = 0
 
-for i in range(n):
-    for j in range(i+1):
-        ans += arr[j]
+for i in range(1, n+1):
+    result += sum(arr[:i])
 
-print(ans)
-
-
-
+print(result)
