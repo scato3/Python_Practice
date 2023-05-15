212a48a8de293c7587d5ef27e54d7499e23f4834
Hyunsu Shin <58941022+scato3@users.noreply.github.com>
2023-05-15 17:39:45 +0900

    [D3] Title: 영준이의 카드 카운팅, Time: 162 ms, Memory: 58,256 KB -BaekjoonHub

--- SWEA/D3/4047. 영준이의 카드 카운팅/영준이의 카드 카운팅.py
+++ SWEA/D3/4047. 영준이의 카드 카운팅/영준이의 카드 카운팅.py
@@ -1,23 +1,27 @@
 t = int(input())
 
 for tc in range(1, t+1):
-    cards = input()
     my_card = {
         'S': [],
         'D': [],
         'H': [],
         'C': []
     }
-    print('#{}'.format(tc), end=' ')
+
+    cards = input()
+
+    print('#{}'.format(tc),end=' ')
     for i in range(0, len(cards), 3):
-        c = cards[i]
+        card = cards[i]
         n = cards[i+1:i+3]
 
-        if n not in my_card[c]:
-            my_card[c].append(n)
+        if n not in my_card[card]:
+            my_card[card].append(n)
         else:
-            print('ERROR')
+            print('ERROR',end=' ')
+            print()
             break
+
     else:
         for key, value in my_card.items():
             print(13 - len(value),end=' ')
